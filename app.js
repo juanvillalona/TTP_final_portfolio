507c02aa89f0a59fea77329a5294197d14226385
Juan Villalona <juan.villalona@lc.cuny.edu>
2018-06-19 15:14:00 -0400

    Added styling and JS to blog tab along with a database functionality

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 
 	var express = require("express"); 
+	var parser = require('body-parser');
+
 	var app = express(); 
 	app.use(express.static('public')); 
 	app.set('view engine', 'ejs');
-	
+
+	app.use(parser.json());
+
+	app.use(parser.urlencoded({extended: true}));
+	app.use(parser.text());
+	app.set('view engine', 'ejs');
+
+	const {Pool,Client} = require('pg');
+
+	const pool = new Pool({
+	user: 'postgres',
+	host:'localhost',
+	database: 'portfolio',
+	password: 'Yunaelin4',
+	port: 5432,
+	});
+
+	var blog;
+	pool.query('SELECT * FROM blog', (err, res) => {
+		blog = res.rows;
+	})
+		
 	
 	
 	app.get('/', function(req, res){       
@@ -21,6 +44,22 @@
 	app.get('/novel', function(req, res) {
 		res.render('novel');
 	});
+
+	app.get('/blog', function(req, res) {
+		res.render('blog', {blog:blog});
+	});
+
+	app.post('/submit', function(req, res) {
+		var t = req.body;
+		var d = new Date();
+		var month = d.getMonth() + 1;
+		var day = d.getDate();
+		var date = d.getFullYear() + '/' + (month < 10 ? '0' : '') + month +'/' + (day < 10 ? '0' : '') + day;
+		var time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+		pool.query(`INSERT INTO blog(date, time, poster, topic, content) VALUES('${date}' ,  '${time}' , 'Me', '${t.topic}', '${t.subject}')`);
+		res.redirect('blog');
+	});
+
 
 	app.listen(8080);
 	console.log("listening in port 8080");
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready( function (){
 
-    $('#container').animate({opacity: 0}, 0).css({'background-image': 'url(../res/images/fantasy.jpg)'}).animate({opacity: 1}, 2500);
+    $('#container').animate({opacity: 0}, 0).css({'background-image': ''}).animate({opacity: 1}, 2500);
 
 });
